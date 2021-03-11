bb36933f021187c79d85f76a12620a407e2601dd
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-03-12 00:53:48 +0900

    client 기본 UI 구현
    client 기본 UI 구현, 기능은 미구현

--- win socket chat management/resource.h
+++ win socket chat management/resource.h
@@ -3,14 +3,41 @@
 // win socket chat management.rc에서 사용되고 있습니다.
 //
 #define IDD_DIALOG_MAIN                 101
+#define IDD_DIALOG_SIGNUP               103
+#define IDD_DIALOG_CHAT                 105
+#define ID_LOGIN_BTN                    1001
+#define ID_GOTO_SIGNUP_BTN              1002
+#define IDC_STATIC_ID                   1005
+#define IDC_STATIC_PW                   1006
+#define IDC_STATIC_ID2                  1006
+#define IDC_EDIT_SIGNUP_ID              1007
+#define IDC_EDIT_SIGNUP_PW2             1008
+#define IDC_EDIT_SIGNUP_PW              1009
+#define IDC_STATIC_ID3                  1009
+#define IDC_STATIC_SIGNUP_PW2           1009
+#define IDC_EDIT_SIGNUP_NICKNAME        1010
+#define IDC_STATIC_SIGNUP_ID            1011
+#define IDC_EDIT_ID                     1012
+#define IDC_EDIT_PW                     1013
+#define IDC_STATIC_ID4                  1013
+#define IDC_STATIC_SIGNUP_NICKNAME      1013
+#define ID_SIGNUP_OK_BTN                1014
+#define IDC_STATIC_SIGNUP_PW            1015
+#define IDC_FRIEND_BTN                  1017
+#define IDC_ADDFRIEND_BTN               1018
+#define IDC_CHAT_BTN2                   1019
+#define IDC_STATIC_MYNAME               1021
+#define IDC_STATIC_MYNAME_IS            1022
+#define IDC_LIST_FRIENDS                1023
+#define IDC_STATIC_GROUP_MENU           1024
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1025
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
