3c6f514428ba10642e79a11eb2576f099ed080af
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-03-11 23:29:31 +0900

    create project
    프로젝트 생성 및 초기 설정

---
+++ win socket chat management/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <Windows.h>
+#include "resource.h"
+
+BOOL CALLBACK MainDlgProc(HWND hDlg, UINT iMessage, WPARAM wParam, LPARAM lParam);
+HINSTANCE g_hInst;
+HWND hDlgMain, hStatic;
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParma, int nCmdShow)
+
+{
+	g_hInst = hInstance;
+	DialogBox(g_hInst, MAKEINTRESOURCE(IDD_DIALOG_MAIN), HWND_DESKTOP, MainDlgProc);
+
+	return 0;
+}
+
+BOOL CALLBACK MainDlgProc(HWND hDlg, UINT iMessage, WPARAM wParam, LPARAM lParam)
+
+{
+	switch (iMessage)
+	{
+	case WM_INITDIALOG:
+		SetWindowPos(hDlg, HWND_TOP, 100, 100, 0, 0, SWP_NOSIZE);
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		case IDCANCEL:
+			EndDialog(hDlgMain, 0);
+			return TRUE;
+		}
+		return FALSE;
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		return TRUE;
+	}
+	return FALSE;
+}
